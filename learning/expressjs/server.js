dde2927fdd3e04c404f886412725e070e633c267
jasmin.martens <papoujuice@gmail.com>
2025-11-15 15:07:06 +0200

    expressjs finished server tutorial

--- learning/expressjs/server.js
+++ learning/expressjs/server.js
@@ -1,9 +1,35 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
+
+const port = process.env.PORT || 8000;
+
+// get dirname
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use(logger);
 
 // setup static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(8000, () => console.log("Server is running on Port 8000"));
+// routes
+app.use("/api/posts", posts);
+
+app.use(notFound);
+
+// error handler should be declared below routes
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server is running on Port ${port}`));
