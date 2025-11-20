2d733e6daf931126d7370aa63169b400a8119f54
jasmin.martens <papoujuice@gmail.com>
2025-11-20 16:21:30 +0200

    finished react crash course

--- learning/react-crash-course/src/App.jsx
+++ learning/react-crash-course/src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 // return always has to be a single element (nesting depth doesn't matter)
 // fragments can be used, though so:
@@ -8,19 +16,51 @@ import React from 'react'
 // </>
 // would be valid 
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newJob)
+    })
+    return;
+  }
 
-  const names = ["tick", "trick", "track"]
-  return (
-    <>
-        <div className='text-5xl'>App</div>
-        <ul>
-          { names.map((name, index) => (
-            <li key={index}>{name}</li>
-          ))}
-        </ul>
-    </>
+  const deleteJob = async(id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE"
+    })
+    return;
+  }
 
+  const updateJob = async(job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(job)
+    })
+    return;
+  }
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      {/*  any route in here is going to use the main layout  */}
+    <Route index element={<HomePage/>}/>
+    <Route path="/jobs" element={<JobsPage/>}/>
+    <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>}/>
+    <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+        <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
+    <Route path="*" element={<NotFoundPage/>}/>
+    </Route>
   )
+)
+
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
